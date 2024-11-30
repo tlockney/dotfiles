2a975546b0e4fea69534f06b53cabd0164f43485
Thomas Lockney <thomas@lockney.net>
2024-11-29 22:30:40 -0800

    Added wezterm config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,23 +1,22 @@
 local wezterm = require("wezterm")
 
-config = wezterm.config_builder()
-
-config = {
-	automatically_reload_config = true,
-	enable_tab_bar = false,
-	window_close_confirmation = "NeverPrompt",
-	window_decorations = "RESIZE",
-	-- default_cursor_style = "BlinkingBar",
-	color_scheme = "Nord (Gogh)",
-	-- font = wezterm.font("FiraCode Nerd Font Mono"),
-	font = wezterm.font("FiraCode Nerd Font Mono", {weight=450, stretch="Normal", style="Normal"}),
-	font_size = 16,
-	window_padding = {
-		left = 5,
-		right = 5,
-		top = 2,
-		bottom = 2,
-	},
+local config = {}
+config.automatically_reload_config = true
+config.enable_tab_bar = false
+config.window_close_confirmation = "NeverPrompt"
+config.window_decorations = "RESIZE"
+-- config.default_cursor_style = "BlinkingBar"
+config.color_scheme = "Nord (Gogh)"
+-- config.font = wezterm.font("FiraCode Nerd Font Mono")
+config.font = wezterm.font("FiraCode Nerd Font Mono", {weight=450, stretch="Normal", style="Normal"})
+config.font_size = 16
+config.window_padding = {
+	left = 5,
+	right = 5,
+	top = 2,
+	bottom = 2,
 }
+config.automatically_reload_config = true
+config.scrollback_lines = 5000
 
 return config
