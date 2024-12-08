6c43fffc175aa388ddf41a623c7dd3ad0c2e8c20
Thomas Lockney <thomas@lockney.net>
2024-12-08 08:07:15 +0000

    tweaked setup

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -51,13 +51,6 @@
 (column-number-mode)
 (global-display-line-numbers-mode t)
 
-;; Disable line numbers for some modes
-(dolist (mode '(org-mode-hook
-		term-mode-hook))
-    shell-mode-hook
-		eshell-mode-hook
-  (add-hook mode (lambda () (display-line-numbers-mode 0))))
-
 
 ;; Enable mouse
 (require 'mouse)
