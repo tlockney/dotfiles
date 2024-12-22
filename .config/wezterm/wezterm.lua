6532148b66784f53443e4e8a38efa70587817a5a
Thomas Lockney <thomas@lockney.net>
2024-12-21 23:35:05 -0800

    cleanup wezterm config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,22 +1,32 @@
 local wezterm = require("wezterm")
 
-local config = {}
-config.automatically_reload_config = true
--- config.enable_tab_bar = false
-config.window_close_confirmation = "NeverPrompt"
-config.window_decorations = "RESIZE"
--- config.default_cursor_style = "BlinkingBar"
-config.color_scheme = "Nord (Gogh)"
--- config.font = wezterm.font("FiraCode Nerd Font Mono")
-config.font = wezterm.font("FiraCode Nerd Font Mono", {weight=450, stretch="Normal", style="Normal"})
-config.font_size = 16
-config.window_padding = {
+local config = {
+   automatically_reload_config = true,
+
+   -- enable_tab_bar = false,
+   -- default_cursor_style = "BlinkingBar",
+   -- color_scheme = "Nord (Gogh)",
+   color_scheme = 'Catppuccin Mocha',
+
+   font = wezterm.font("FiraCode Nerd Font Mono", {weight=450, stretch="Normal", style="Normal"}),
+   font_size = 16,
+
+   window_close_confirmation = "NeverPrompt",
+   window_decorations = "RESIZE",
+   window_padding = {
 	left = 5,
 	right = 5,
 	top = 2,
 	bottom = 2,
+   },
+   window_background_opacity = 0.70,
+   macos_window_background_blur = 30,
+
+   scrollback_lines = 5000,
 }
-config.automatically_reload_config = true
-config.scrollback_lines = 5000
+
+require("wuake").setup {
+   config = config,
+}
 
 return config
