c8734fe1f24fc40fe808be1257aa4b036a5cc2db
Thomas Lockney <thomas@lockney.net>
2024-12-25 15:36:24 -0800

    Added hotkeys for apps

--- .config/hammerspoon/init.lua
+++ .config/hammerspoon/init.lua
@@ -1,5 +1,7 @@
 hs.logger.defaultLogLevel = 'debug'
 
+hotkeyLogger = hs.logger.new('hotkey', 'info')
+
 wuakeLog = hs.logger.new('wuake', 'info')
 
 function reloadConfig(files)
@@ -13,21 +15,42 @@ function reloadConfig(files)
 	hs.reload()
     end
 end
-myWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
+
+myWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.config/hammerspoon/", reloadConfig):start()
 hs.alert.show("Config loaded")
 
-hs.hotkey.bind({ "Alt" }, "`", function()
-      wezTask = hs.task.new("/opt/homebrew/bin/wezterm",
-		  nil,
-		  function(task, stdOut, stdErr)
-		     if not stdErr == '' then
-			wuakeLog.e("error:" .. stdErr)
+function launchAppOnHotkey(modifiers, key, appName)
+   hs.hotkey.bind(modifiers, key, function()
+		     local app = hs.application.find(appName)
+		     if app == nil then
+			hs.application.launchOrFocus(appName)
+		     elseif app:isFrontmost() then
+			app:hide()
+		     else
+			local win = app:mainWindow()
+			spaces.moveWindowToSpace(win:id(), spaces.activeSpaceOnScreen())
+			win:focus()
 		     end
-		     if not stdOut == '' then
-			wuakeLog.i(stdOut)
-		     end
-		     return true
-		  end,
-		  {"connect", "wuake"})
-      wezTask:start()
-end)
+   end)
+end
+
+launchAppOnHotkey({"cmd"},"`","WezTerm")
+launchAppOnHotkey({"alt","shift"},"o","Obsidian")
+
+-- hs.hotkey.bind({ "Alt" }, "`", function()
+--       wezTask = hs.task.new("/opt/homebrew/bin/wezterm",
+--		  nil,
+--		  function(task, stdOut, stdErr)
+--		     if not stdErr == '' then
+--			wuakeLog.e("error:" .. stdErr)
+--		     end
+--		     if not stdOut == '' then
+--			wuakeLog.i(stdOut)
+--		     end
+--		     return true
+--		  end,
+--		  {"connect", "wuake"})
+--       wezTask:start()
+-- end)
+
+spaces = require("hs.spaces")
