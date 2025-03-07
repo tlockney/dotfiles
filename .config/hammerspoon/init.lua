0d1b0e8f91ad67517de074828fe2c1d7ceb5a25c
Thomas Lockney <thomas@lockney.net>
2025-03-07 15:41:16 -0800

    minor cleanup

--- .config/hammerspoon/init.lua
+++ .config/hammerspoon/init.lua
@@ -1,10 +1,18 @@
-hs.logger.defaultLogLevel = 'info'
+require("hs.ipc")
+require("button_images")
+require("util")
+
+-- require("app-watcher")
+
+hs.logger.defaultLogLevel = 'debug'
 hs.notify.new({ informativeText = "Loading config" }):send()
 
-require("hs.ipc")
+hs.hotkey.bind({ "ctrl", "alt", "cmd" }, "H", function()
+	hs.toggleConsole()
+end)
 
 function reloadConfig(files)
-	doReload = false
+	local doReload = false
 	for _, file in pairs(files) do
 		if file:sub(-4) == ".lua" then
 			doReload = true
@@ -16,9 +24,9 @@ function reloadConfig(files)
 end
 
 local hsConfigPath = os.getenv("HOME") .. "/.config/hammerspoon/"
-myWatcher = hs.pathwatcher.new(hsConfigPath, reloadConfig):start()
+hs.pathwatcher.new(hsConfigPath, reloadConfig):start()
 
-spaces = require("hs.spaces")
+local spaces = require("hs.spaces")
 
 function launchAppOnHotkey(modifiers, key, appName)
 	hs.hotkey.bind(modifiers, key, function()
@@ -42,35 +50,69 @@ hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "W", function()
 	hs.notify.new({ informativeText = "Hello World" }):send()
 end)
 
--- require("app-watcher")
+local sdlog = hs.logger.new("streamdeck")
 
-require("button_images")
+local thunderboltButton = {
+	position = 1,
+	image = streamdeck_imageFromText('⚡'),
+	onpress = function()
+		sdlog.i("Thunderbolt")
+	end,
+}
 
-sdlog = hs.logger.new("streamdeck")
+local hdmiButton = {
+	position = 2,
+	image = hs.image.imageFromPath(hs.configdir .. "/images/hdmi2.png"),
+	onpress = function()
+		sdlog.i("hdmi")
+	end
+}
+local monitorToggleButton = {
+	position = 3,
+	image = streamdeck_imageFromText('🖥️'),
+	onpress = function()
+		sdlog.i("Toggle monitor")
+	end
+}
 
-streamdecks = {}
-streamdecks['A00WA4121N7XWN'] = {
-	buttons = {
-
+local streamdecks = {
+	['A00WA4121N7XWN'] = {},
+	['DL50K1A44132'] = {
+		buttons = {
+			thunderboltButton,
+			hdmiButton,
+			monitorToggleButton
+		}
 	}
 }
 
-function initStreamdeck(connected, device)
+hs.streamdeck.init(function(connected, device)
+	sdlog.i(hs.inspect(device))
 	if streamdecks[device:serialNumber()] then
-		device:reset()
-		columns, rows = device:buttonLayout()
-		numButtons = columns * rows
-		device:setButtonImage(1, streamdeck_imageFromText('🖥️'))
-		sdlog.i(hs.inspect(device))
-		device:buttonCallback(function(sd, num, pressed)
-			-- sdlog.i(hs.inspect(sd))
-			-- sdlog.i(num)
-			-- sdlog.i(pressed)
-			if pressed then
-
+		local deck = streamdecks[device:serialNumber()]
+		local columns, rows = device:buttonLayout()
+		if deck.buttons ~= nil then
+			local buttons = deck.buttons
+			local onpress = {}
+			local onrelease = {}
+			device:reset()
+			for idx = 1, #buttons do
+				local button = buttons[idx]
+				local position = button.position
+				onpress[position] = button.onpress
+				onrelease[position] = button.onrelease
+				device:setButtonImage(position, button.image)
 			end
-		end)
+			device:buttonCallback(function(sd, num, pressed)
+				if onpress[num] ~= nil and pressed then
+					sdlog.d("Button " .. num .. " pressed.")
+					onpress[num]()
+				end
+				if onrelease[num] ~= nil and not pressed then
+					sdlog.d("Button " .. num .. " released.")
+					onrelease[num]()
+				end
+			end)
+		end
 	end
-end
-
-hs.streamdeck.init(initStreamdeck)
+end)
