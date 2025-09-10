d53a89eb5da8c2f16aa9b1662a5b49d7d20fb3c8
Thomas Lockney <thomas@lockney.net>
2025-09-10 08:05:28 -0700

    Updated WezTerm settings

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,21 +1,45 @@
-local function color_scheme_for_appearance(appearance)
-   if appearance:find "Dark" then
-      return "Catppuccin Mocha"
-   else
-      return "Catppuccin Latte"
-   end
-end
-
 local wezterm = require("wezterm")
-local config = wezterm.config_builder()
 local action = wezterm.action
+local appearance = require 'appearance'
+
+wezterm.on('update-status', function(window)
+  -- Grab the utf8 character for the "powerline" left facing
+  -- solid arrow.
+  local SOLID_LEFT_ARROW = utf8.char(0xe0b2)
+
+  -- Grab the current window's configuration, and from it the
+  -- palette (this is the combination of your chosen colour scheme
+  -- including any overrides).
+  local color_scheme = window:effective_config().resolved_palette
+  local bg = color_scheme.background
+  local fg = color_scheme.foreground
+
+  window:set_right_status(wezterm.format({
+    -- First, we draw the arrow...
+    { Background = { Color = 'none' } },
+    { Foreground = { Color = bg } },
+    { Text = SOLID_LEFT_ARROW },
+    -- Then we draw our text
+    { Background = { Color = bg } },
+    { Foreground = { Color = fg } },
+    { Text = ' ' .. wezterm.hostname() .. ' ' },
+  }))
+end)
+
+local config = wezterm.config_builder()
+
+if appearance.is_dark() then
+    -- config.color_scheme = 'Catppuccin Mocha'
+    config.color_scheme = 'Tokyo Night'
+else
+    -- config.color_scheme = 'Catppuccin Latte'
+    config.color_scheme = 'Tokyo Night Day'
+end
 
 config.automatically_reload_config = true
 config.hide_tab_bar_if_only_one_tab = false
 config.use_fancy_tab_bar = true
---  config.tab_max_width = 80
-config.color_scheme = color_scheme_for_appearance(wezterm.gui.get_appearance())
-config.native_macos_fullscreen_mode = false
+config.native_macos_fullscreen_mode = true
 config.font = wezterm.font_with_fallback {
   "JetBrainsMono Nerd Font",
   "FiraCode Nerd Font Mono"
@@ -38,18 +62,38 @@ config.window_frame = {
   font_size = 16,
 }
 config.window_background_opacity = 0.90
-config.macos_window_background_blur = 20
+config.macos_window_background_blur = 30
 config.scrollback_lines = 5000
+config.set_environment_variables = {
+  PATH = '/opt/homebrew/bin:' .. os.getenv('PATH')
+}
 
 config.keys = {
-  { key = 'd',          mods = 'CMD|SHIFT', action = action.SplitVertical { domain = 'CurrentPaneDomain' } },
-  { key = 'd',          mods = 'CMD',       action = action.SplitHorizontal { domain = 'CurrentPaneDomain' } },
-  { key = 'k',          mods = 'CMD',       action = action.ClearScrollback 'ScrollbackAndViewport' },
-  { key = 'w',          mods = 'CMD',       action = action.CloseCurrentPane { confirm = false } },
-  { key = 'w',          mods = 'CMD|SHIFT', action = action.CloseCurrentTab { confirm = false } },
-  { key = 'LeftArrow',  mods = 'CMD',       action = action.SendKey { key = 'Home' } },
-  { key = 'RightArrow', mods = 'CMD',       action = action.SendKey { key = 'End' } },
+  { key = 'LeftArrow',  mods = 'OPT',       action = action.SendString('\x1bb') },
+  { key = 'RightArrow', mods = 'OPT',       action = action.SendString('\x1bf') },
+  { key = '{',          mods = 'SHIFT|ALT', action = action.MoveTabRelative(-1) },
+  { key = '}',          mods = 'SHIFT|ALT', action = action.MoveTabRelative(1)  },
   { key = 'p',          mods = 'CMD|SHIFT', action = action.ActivateCommandPalette },
+  { key = ',',          mods = 'SUPER',     action = action.SpawnCommandInNewTab {
+    cwd = wezterm.home_dir,
+    args = { 'emacs', '-nw', wezterm.config_file },
+  }},
+    -- Vertical pipe (|) -> horizontal split
+  {
+    key = '\\',
+    mods = 'CMD|SHIFT',
+    action = wezterm.action.SplitHorizontal {
+      domain = 'CurrentPaneDomain'
+    },
+  },
+  -- Underscore (_) -> vertical split
+  {
+    key = '-',
+    mods = 'CMD|SHIFT',
+    action = wezterm.action.SplitVertical {
+      domain = 'CurrentPaneDomain'
+    },
+  },
 }
 
 return config
