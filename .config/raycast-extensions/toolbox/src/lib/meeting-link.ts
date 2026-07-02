767829556a2a6d492b7a0014286e0631309efb43
Thomas Lockney <thomas@lockney.net>
2026-07-02 01:55:22 -0700

    raycast: match scheduled Webex join URLs in meeting-link detection

--- .config/raycast-extensions/toolbox/src/lib/meeting-link.ts
+++ .config/raycast-extensions/toolbox/src/lib/meeting-link.ts
@@ -1,6 +1,6 @@
 // Matches the join URLs of the conferencing services we actually encounter.
 const MEETING_URL_PATTERN =
-  /https?:\/\/[^\s<>"']*(?:zoom\.us\/j\/|meet\.google\.com\/|teams\.microsoft\.com\/l\/meetup-join|webex\.com\/(?:meet|join)\/)[^\s<>"']*/i;
+  /https?:\/\/[^\s<>"']*(?:zoom\.us\/j\/|meet\.google\.com\/|teams\.microsoft\.com\/l\/meetup-join|webex\.com\/(?:meet\/|join\/|[^\s<>"']*j\.php))[^\s<>"']*/i;
 
 export function extractMeetingUrl(
   ...sources: Array<string | undefined>
